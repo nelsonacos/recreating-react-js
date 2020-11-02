e25383a883a7b826a76c76bb2def0e17b36e1030
Nelson Acosta <nelson.acosta.web@gmail.com>
2020-11-01 22:52:02 -0300

    feat(component folder, lib folder): añadiendo soporte a dark mode

--- components/user-styled.js
+++ components/user-styled.js
@@ -1,9 +1,10 @@
 import styled from '../lib/styled-components.js'
 import { Component, createElement } from '../lib/react/index.js'
 
-
 const UserStyled = styled.div`
-  background-image: linear-gradient(to bottom, #f9f9f9 0%, #f9f9f9 130px,rgba(0,0,0,.15) 130px, rgba(0,0,0,.15) 131px, white 131px, white 100%);
+  background-image: linear-gradient(to bottom, ${({ primaryColor }) => primaryColor} 0%, ${({ primaryColor }) => primaryColor} 130px,${({ tertiaryColor }) => tertiaryColor} 130px, ${({ tertiaryColor }) => tertiaryColor} 131px, ${({ secondaryColor }) => secondaryColor} 131px, ${({ secondaryColor}) => secondaryColor} 100%);
+  /* background: ${(props) => props.primaryColor}; */
+  color: ${({ fontColor }) => fontColor} ;
   text-align: center;
   overflow: hidden;
   padding: 20px;
@@ -14,29 +15,62 @@ const UserStyled = styled.div`
   user-select: none;
 `
 
-const AvatarStyled = styled.div`
-    height: 150px;
-    width: 150px;
-    border: 5px solid white;
-    box-shadow: 0 0 2px black;
-    background-repeat: no-repeat;
-    background-position: 50%;
-    border-radius: 50%;
-    background-size: cover;
-    margin: 0 auto;
-    background-image: url(./images/nelson-acosta.jpg);
+const AvatarStyled = styled.img`
+  max-width: 150px;
+  border-radius: 50%;
+  border: 5px solid white;
+  box-shadow: 0 0 2px black;
 `
-
-class User extends Component {
-    render() {
-        const { name, avatar } = this.props
-        return UserStyled({
-            children: [
-                AvatarStyled({}),
-                createElement('h2', null, name) 
-            ]
-        }, '')
-    }
+const theme = {
+  light: {
+    primaryColor: '#f9f9f9',
+    secondaryColor: 'white',
+    tertiaryColor: 'rgba(0,0,0,.15)',
+    fontColor: 'black',
+  },
+  dark: {
+    primaryColor: '#212429',
+    secondaryColor: '#212429',
+    tertiaryColor: 'white',
+    fontColor: 'white',
+  }
 }
 
-export default User 
+class User extends Component {
+  state = {
+    mode: 'light'
+  }
+  setMode = (event) => {
+    // console.log(event.matches)
+    if(event.matches) {
+      return this.setState({
+        mode: 'dark'
+      })
+    }
+    this.setState({
+      mode: 'light'
+    })
+  }
+  componentDidMount() {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    this.setMode(mediaQuery)
+    mediaQuery.addEventListener('change', this.setMode)
+  }
+  render() {
+    const { mode } = this.state
+    const colors = (mode === 'light') ? theme.light : theme.dark
+    const { name, avatar } = this.props
+    console.log(colors)
+    return UserStyled({
+      ...colors,
+      children: [
+        AvatarStyled({
+          src: avatar
+        }),
+        createElement('h2', null, `${name} ${mode} mode`)
+      ]
+    }, '')
+  }
+}
+
+export default User
